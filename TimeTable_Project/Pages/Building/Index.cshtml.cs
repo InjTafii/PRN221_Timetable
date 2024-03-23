13a1aead00e45bec5c56360ee886156fd2ea5ef8
unknown <mittoteo0608@gmail.com>
2024-03-24 01:46:38 +0700

    Generate Schedule

---
+++ TimeTable_Project/Pages/Building/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using TimeTable_Project.Models;
+
+namespace TimeTable_Project.Pages.Building
+{
+    public class IndexModel : PageModel
+    {
+        private readonly TimeTable_Project.Models.ScheduleManagementContext _context;
+
+        public IndexModel(TimeTable_Project.Models.ScheduleManagementContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Models.Building> Building { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Buildings != null)
+            {
+                Building = await _context.Buildings.ToListAsync();
+            }
+        }
+    }
+}
